a1c2363485ebc419aed5eb30a413f351a2bcef38
lynnhollins <140206594+lynnhollins@users.noreply.github.com>
2023-08-23 16:40:57 -0600

    Commit Initial Files
    Scoreboard unfinished, need to complete CSS

---
+++ index.js
@@ -0,0 +1,44 @@
+let homeEl = document.getElementById("home-el");
+let guestEl = document.getElementById("guest-el");
+let homeScore = 0;
+let guestScore = 0;
+
+function homeAdd1() {
+    homeScore += 1;
+    homeEl.textContent = homeScore;
+}
+
+function homeAdd2() {
+    homeScore += 2;
+    homeEl.textContent = homeScore;
+}
+
+function homeAdd3() {
+    homeScore += 3;
+    homeEl.textContent = homeScore;
+}
+function resetScore() {
+    homeScore = 0;
+    guestScore = 0;
+    homeEl.textContent = 0;
+    guestEl.textContent = 0;
+}
+
+function guestAdd1() {
+    guestScore += 1;
+    guestEl.textContent = guestScore;
+}
+
+function guestAdd2() {
+    guestScore += 2;
+    guestEl.textContent = guestScore;
+}
+
+function guestAdd3() {
+    guestScore += 3;
+    guestEl.textContent = guestScore;
+}
+function guestReset() {
+    guestScore = 0;
+    guestEl.textContent = 0;
+}
